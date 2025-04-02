ff27aefddb315d3f400457682bd4b49862901031
diozenio <d.dionisiobsl@gmail.com>
2025-04-02 20:26:21 -0300

    fix(Expenses): Update required error message when creating categories

--- src/modules/expenses/transactions/CreateTransationsCategory/CreateTransationsCategoryParams.ts
+++ src/modules/expenses/transactions/CreateTransationsCategory/CreateTransationsCategoryParams.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createTransactionCategoryParams = z.object({
   name: z
-    .string()
+    .string({ required_error: "Name is required" })
     .min(1, "Name is required")
     .max(15, "Name must be less than 15 characters"),
 });
