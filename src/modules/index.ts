3489ce544de414a52629fe0e28a4178cc05cfb23
diozenio <d.dionisiobsl@gmail.com>
2025-04-03 17:19:34 -0300

    refactor(Expenses): Move Transactions Categories to categories folder

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,9 +1,9 @@
 import { HelloController } from "./hello/HelloController";
 import { GetTransactionsController } from "./expenses/transactions/GetTransactions/GetTransactionsController";
-import { GetTransactionsCategoriesController } from "./expenses/transactions/GetTransactionsCategories/GetTransactionsCategoriesController";
-import { CreateTransactionsCategoryController } from "./expenses/transactions/CreateTransactionsCategory/CreateTransactionsCategoryController";
-import { UpdateTransactionsCategoryController } from "./expenses/transactions/UpdateTransactionsCategory/UpdateTransactionsCategoryController";
-import { DeleteTransactionsCategoryController } from "./expenses/transactions/DeleteTransactionsCategory/DeleteTransactionsCategoryController";
+import { GetTransactionsCategoriesController } from "./expenses/transactions/categories/GetTransactionsCategories/GetTransactionsCategoriesController";
+import { CreateTransactionsCategoryController } from "./expenses/transactions/categories/CreateTransactionsCategory/CreateTransactionsCategoryController";
+import { UpdateTransactionsCategoryController } from "./expenses/transactions/categories/UpdateTransactionsCategory/UpdateTransactionsCategoryController";
+import { DeleteTransactionsCategoryController } from "./expenses/transactions/categories/DeleteTransactionsCategory/DeleteTransactionsCategoryController";
 
 export {
   HelloController,
